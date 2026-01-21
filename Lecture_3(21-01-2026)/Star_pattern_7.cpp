3564d6412948fb620cf1f187efa00d56c745670c
janvi gupta <janvigupta7000@gmail.com>
2026-01-21 21:47:17 +0530

    Add files via upload

---
+++ Lecture_3(21-01-2026)/Star_pattern_7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int i=0;i<n;i++)
+        cout<<"* ";
+        cout<<endl;
+        for(int i=1;i<n-1;i++){
+        cout<<"*";
+            for(int j=0;j<2*n-3;j++) 
+            cout<<" ";
+        cout<<"*"<<endl;
+    }
+    for(int i=0;i<n;i++)
+        cout<<"* ";
+        cout<<endl;
+        return 0;
+
+
+}
---
+++ Lecture_3(21-01-2026)/Star_pattern_8.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for(int i = 0; i < n/2; i++)
+    {
+        for(int j = 0; j < i; j++) 
+        cout << " ";
+        cout << "*";
+
+        for(int k = 0; k < n - 2*i - 2; k++) 
+        cout << " ";
+        cout << "*";
+        cout << endl;
+    }
+
+    for(int i = 0; i < n/2; i++) 
+    cout << " ";
+    cout << "*" << endl;
+
+    for(int i = n/2 - 1; i >= 0; i--)
+    {
+        for(int j = 0; j < i; j++) 
+        cout << " ";
+        cout << "*";
+
+        for(int k = 0; k < n - 2*i - 2; k++) 
+        cout << " ";
+        cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
